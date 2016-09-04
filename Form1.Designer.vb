b9ba7e50f25601f3514bec2184fafe51dd294f85
gay soy <fabioantonio1@hotmail.es>
2016-09-03 19:46:12 -0500

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,421 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LblNombreCancion = New System.Windows.Forms.Label()
+        Me.LblNombreArtista = New System.Windows.Forms.Label()
+        Me.LblAlbum = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.lblAño = New System.Windows.Forms.Label()
+        Me.LblNombreCancionSec1 = New System.Windows.Forms.Label()
+        Me.LblNombreArtistaSec1 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.LblNombreArtistaSec2 = New System.Windows.Forms.Label()
+        Me.LblNombreCancionSec2 = New System.Windows.Forms.Label()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.LblNombreArtistaSec3 = New System.Windows.Forms.Label()
+        Me.LblNombreCancionSec3 = New System.Windows.Forms.Label()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.LblNombreArtistaSec4 = New System.Windows.Forms.Label()
+        Me.LblNombreCancionSec4 = New System.Windows.Forms.Label()
+        Me.Panel5 = New System.Windows.Forms.Panel()
+        Me.LblTotal = New System.Windows.Forms.Label()
+        Me.LblMostrarTotal = New System.Windows.Forms.Label()
+        Me.BtnVerInfo1 = New System.Windows.Forms.Button()
+        Me.BtnCerrar = New System.Windows.Forms.Button()
+        Me.BtnComprar2 = New System.Windows.Forms.Button()
+        Me.BtnVerInfo2 = New System.Windows.Forms.Button()
+        Me.BtnComprar3 = New System.Windows.Forms.Button()
+        Me.BtnVerInfo3 = New System.Windows.Forms.Button()
+        Me.BtnComprar4 = New System.Windows.Forms.Button()
+        Me.BtnVerInfo4 = New System.Windows.Forms.Button()
+        Me.BtnComprar1 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        Me.Panel4.SuspendLayout()
+        Me.Panel5.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'LblNombreCancion
+        '
+        Me.LblNombreCancion.AutoSize = True
+        Me.LblNombreCancion.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblNombreCancion.Location = New System.Drawing.Point(92, 54)
+        Me.LblNombreCancion.Name = "LblNombreCancion"
+        Me.LblNombreCancion.Size = New System.Drawing.Size(159, 24)
+        Me.LblNombreCancion.TabIndex = 0
+        Me.LblNombreCancion.Text = "Nombre Canción:"
+        '
+        'LblNombreArtista
+        '
+        Me.LblNombreArtista.AutoSize = True
+        Me.LblNombreArtista.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblNombreArtista.Location = New System.Drawing.Point(93, 78)
+        Me.LblNombreArtista.Name = "LblNombreArtista"
+        Me.LblNombreArtista.Size = New System.Drawing.Size(91, 15)
+        Me.LblNombreArtista.TabIndex = 1
+        Me.LblNombreArtista.Text = "Nombre Artista:"
+        '
+        'LblAlbum
+        '
+        Me.LblAlbum.AutoSize = True
+        Me.LblAlbum.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblAlbum.Location = New System.Drawing.Point(93, 93)
+        Me.LblAlbum.Name = "LblAlbum"
+        Me.LblAlbum.Size = New System.Drawing.Size(45, 15)
+        Me.LblAlbum.TabIndex = 2
+        Me.LblAlbum.Text = "Album:"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(13, 24)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(74, 124)
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.BtnComprar1)
+        Me.Panel1.Controls.Add(Me.BtnVerInfo1)
+        Me.Panel1.Controls.Add(Me.LblNombreArtistaSec1)
+        Me.Panel1.Controls.Add(Me.LblNombreCancionSec1)
+        Me.Panel1.Location = New System.Drawing.Point(13, 154)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(487, 70)
+        Me.Panel1.TabIndex = 4
+        '
+        'lblAño
+        '
+        Me.lblAño.AutoSize = True
+        Me.lblAño.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAño.Location = New System.Drawing.Point(93, 108)
+        Me.lblAño.Name = "lblAño"
+        Me.lblAño.Size = New System.Drawing.Size(28, 15)
+        Me.lblAño.TabIndex = 5
+        Me.lblAño.Text = "Año"
+        '
+        'LblNombreCancionSec1
+        '
+        Me.LblNombreCancionSec1.AutoSize = True
+        Me.LblNombreCancionSec1.Location = New System.Drawing.Point(14, 15)
+        Me.LblNombreCancionSec1.Name = "LblNombreCancionSec1"
+        Me.LblNombreCancionSec1.Size = New System.Drawing.Size(89, 13)
+        Me.LblNombreCancionSec1.TabIndex = 0
+        Me.LblNombreCancionSec1.Text = "Nombre Canción:"
+        '
+        'LblNombreArtistaSec1
+        '
+        Me.LblNombreArtistaSec1.AutoSize = True
+        Me.LblNombreArtistaSec1.Location = New System.Drawing.Point(14, 41)
+        Me.LblNombreArtistaSec1.Name = "LblNombreArtistaSec1"
+        Me.LblNombreArtistaSec1.Size = New System.Drawing.Size(79, 13)
+        Me.LblNombreArtistaSec1.TabIndex = 1
+        Me.LblNombreArtistaSec1.Text = "Nombre Artista:"
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.BtnComprar2)
+        Me.Panel2.Controls.Add(Me.BtnVerInfo2)
+        Me.Panel2.Controls.Add(Me.LblNombreArtistaSec2)
+        Me.Panel2.Controls.Add(Me.LblNombreCancionSec2)
+        Me.Panel2.Location = New System.Drawing.Point(12, 230)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(487, 70)
+        Me.Panel2.TabIndex = 5
+        '
+        'LblNombreArtistaSec2
+        '
+        Me.LblNombreArtistaSec2.AutoSize = True
+        Me.LblNombreArtistaSec2.Location = New System.Drawing.Point(14, 41)
+        Me.LblNombreArtistaSec2.Name = "LblNombreArtistaSec2"
+        Me.LblNombreArtistaSec2.Size = New System.Drawing.Size(79, 13)
+        Me.LblNombreArtistaSec2.TabIndex = 1
+        Me.LblNombreArtistaSec2.Text = "Nombre Artista:"
+        '
+        'LblNombreCancionSec2
+        '
+        Me.LblNombreCancionSec2.AutoSize = True
+        Me.LblNombreCancionSec2.Location = New System.Drawing.Point(14, 15)
+        Me.LblNombreCancionSec2.Name = "LblNombreCancionSec2"
+        Me.LblNombreCancionSec2.Size = New System.Drawing.Size(89, 13)
+        Me.LblNombreCancionSec2.TabIndex = 0
+        Me.LblNombreCancionSec2.Text = "Nombre Canción:"
+        '
+        'Panel3
+        '
+        Me.Panel3.Controls.Add(Me.BtnComprar3)
+        Me.Panel3.Controls.Add(Me.BtnVerInfo3)
+        Me.Panel3.Controls.Add(Me.LblNombreArtistaSec3)
+        Me.Panel3.Controls.Add(Me.LblNombreCancionSec3)
+        Me.Panel3.Location = New System.Drawing.Point(13, 306)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(487, 70)
+        Me.Panel3.TabIndex = 5
+        '
+        'LblNombreArtistaSec3
+        '
+        Me.LblNombreArtistaSec3.AutoSize = True
+        Me.LblNombreArtistaSec3.Location = New System.Drawing.Point(14, 41)
+        Me.LblNombreArtistaSec3.Name = "LblNombreArtistaSec3"
+        Me.LblNombreArtistaSec3.Size = New System.Drawing.Size(79, 13)
+        Me.LblNombreArtistaSec3.TabIndex = 1
+        Me.LblNombreArtistaSec3.Text = "Nombre Artista:"
+        '
+        'LblNombreCancionSec3
+        '
+        Me.LblNombreCancionSec3.AutoSize = True
+        Me.LblNombreCancionSec3.Location = New System.Drawing.Point(14, 15)
+        Me.LblNombreCancionSec3.Name = "LblNombreCancionSec3"
+        Me.LblNombreCancionSec3.Size = New System.Drawing.Size(89, 13)
+        Me.LblNombreCancionSec3.TabIndex = 0
+        Me.LblNombreCancionSec3.Text = "Nombre Canción:"
+        '
+        'Panel4
+        '
+        Me.Panel4.Controls.Add(Me.BtnComprar4)
+        Me.Panel4.Controls.Add(Me.BtnVerInfo4)
+        Me.Panel4.Controls.Add(Me.LblNombreArtistaSec4)
+        Me.Panel4.Controls.Add(Me.LblNombreCancionSec4)
+        Me.Panel4.Location = New System.Drawing.Point(12, 382)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(487, 70)
+        Me.Panel4.TabIndex = 6
+        '
+        'LblNombreArtistaSec4
+        '
+        Me.LblNombreArtistaSec4.AutoSize = True
+        Me.LblNombreArtistaSec4.Location = New System.Drawing.Point(14, 41)
+        Me.LblNombreArtistaSec4.Name = "LblNombreArtistaSec4"
+        Me.LblNombreArtistaSec4.Size = New System.Drawing.Size(79, 13)
+        Me.LblNombreArtistaSec4.TabIndex = 1
+        Me.LblNombreArtistaSec4.Text = "Nombre Artista:"
+        '
+        'LblNombreCancionSec4
+        '
+        Me.LblNombreCancionSec4.AutoSize = True
+        Me.LblNombreCancionSec4.Location = New System.Drawing.Point(14, 15)
+        Me.LblNombreCancionSec4.Name = "LblNombreCancionSec4"
+        Me.LblNombreCancionSec4.Size = New System.Drawing.Size(89, 13)
+        Me.LblNombreCancionSec4.TabIndex = 0
+        Me.LblNombreCancionSec4.Text = "Nombre Canción:"
+        '
+        'Panel5
+        '
+        Me.Panel5.Controls.Add(Me.LblMostrarTotal)
+        Me.Panel5.Controls.Add(Me.LblTotal)
+        Me.Panel5.Location = New System.Drawing.Point(12, 458)
+        Me.Panel5.Name = "Panel5"
+        Me.Panel5.Size = New System.Drawing.Size(487, 70)
+        Me.Panel5.TabIndex = 7
+        '
+        'LblTotal
+        '
+        Me.LblTotal.AutoSize = True
+        Me.LblTotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblTotal.Location = New System.Drawing.Point(12, 21)
+        Me.LblTotal.Name = "LblTotal"
+        Me.LblTotal.Size = New System.Drawing.Size(83, 31)
+        Me.LblTotal.TabIndex = 0
+        Me.LblTotal.Text = "Total:"
+        '
+        'LblMostrarTotal
+        '
+        Me.LblMostrarTotal.AutoSize = True
+        Me.LblMostrarTotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblMostrarTotal.Location = New System.Drawing.Point(378, 21)
+        Me.LblMostrarTotal.Name = "LblMostrarTotal"
+        Me.LblMostrarTotal.Size = New System.Drawing.Size(56, 25)
+        Me.LblMostrarTotal.TabIndex = 1
+        Me.LblMostrarTotal.Text = "S/. 0"
+        '
+        'BtnVerInfo1
+        '
+        Me.BtnVerInfo1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.BtnVerInfo1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnVerInfo1.Location = New System.Drawing.Point(313, 31)
+        Me.BtnVerInfo1.Name = "BtnVerInfo1"
+        Me.BtnVerInfo1.Size = New System.Drawing.Size(75, 23)
+        Me.BtnVerInfo1.TabIndex = 2
+        Me.BtnVerInfo1.Text = "Ver Info"
+        Me.BtnVerInfo1.UseVisualStyleBackColor = False
+        '
+        'BtnCerrar
+        '
+        Me.BtnCerrar.BackColor = System.Drawing.Color.Gray
+        Me.BtnCerrar.FlatAppearance.BorderSize = 0
+        Me.BtnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnCerrar.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnCerrar.Location = New System.Drawing.Point(470, 12)
+        Me.BtnCerrar.Name = "BtnCerrar"
+        Me.BtnCerrar.Size = New System.Drawing.Size(30, 30)
+        Me.BtnCerrar.TabIndex = 4
+        Me.BtnCerrar.Text = "X"
+        Me.BtnCerrar.UseVisualStyleBackColor = False
+        '
+        'BtnComprar2
+        '
+        Me.BtnComprar2.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.BtnComprar2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnComprar2.Location = New System.Drawing.Point(395, 31)
+        Me.BtnComprar2.Name = "BtnComprar2"
+        Me.BtnComprar2.Size = New System.Drawing.Size(75, 23)
+        Me.BtnComprar2.TabIndex = 5
+        Me.BtnComprar2.Text = "Comprar"
+        Me.BtnComprar2.UseVisualStyleBackColor = False
+        '
+        'BtnVerInfo2
+        '
+        Me.BtnVerInfo2.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.BtnVerInfo2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnVerInfo2.Location = New System.Drawing.Point(314, 31)
+        Me.BtnVerInfo2.Name = "BtnVerInfo2"
+        Me.BtnVerInfo2.Size = New System.Drawing.Size(75, 23)
+        Me.BtnVerInfo2.TabIndex = 4
+        Me.BtnVerInfo2.Text = "Ver Info"
+        Me.BtnVerInfo2.UseVisualStyleBackColor = False
+        '
+        'BtnComprar3
+        '
+        Me.BtnComprar3.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.BtnComprar3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnComprar3.Location = New System.Drawing.Point(394, 31)
+        Me.BtnComprar3.Name = "BtnComprar3"
+        Me.BtnComprar3.Size = New System.Drawing.Size(75, 23)
+        Me.BtnComprar3.TabIndex = 5
+        Me.BtnComprar3.Text = "Comprar"
+        Me.BtnComprar3.UseVisualStyleBackColor = False
+        '
+        'BtnVerInfo3
+        '
+        Me.BtnVerInfo3.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.BtnVerInfo3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnVerInfo3.Location = New System.Drawing.Point(313, 31)
+        Me.BtnVerInfo3.Name = "BtnVerInfo3"
+        Me.BtnVerInfo3.Size = New System.Drawing.Size(75, 23)
+        Me.BtnVerInfo3.TabIndex = 4
+        Me.BtnVerInfo3.Text = "Ver Info"
+        Me.BtnVerInfo3.UseVisualStyleBackColor = False
+        '
+        'BtnComprar4
+        '
+        Me.BtnComprar4.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.BtnComprar4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnComprar4.Location = New System.Drawing.Point(395, 31)
+        Me.BtnComprar4.Name = "BtnComprar4"
+        Me.BtnComprar4.Size = New System.Drawing.Size(75, 23)
+        Me.BtnComprar4.TabIndex = 5
+        Me.BtnComprar4.Text = "Comprar"
+        Me.BtnComprar4.UseVisualStyleBackColor = False
+        '
+        'BtnVerInfo4
+        '
+        Me.BtnVerInfo4.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.BtnVerInfo4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnVerInfo4.Location = New System.Drawing.Point(314, 31)
+        Me.BtnVerInfo4.Name = "BtnVerInfo4"
+        Me.BtnVerInfo4.Size = New System.Drawing.Size(75, 23)
+        Me.BtnVerInfo4.TabIndex = 4
+        Me.BtnVerInfo4.Text = "Ver Info"
+        Me.BtnVerInfo4.UseVisualStyleBackColor = False
+        '
+        'BtnComprar1
+        '
+        Me.BtnComprar1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.BtnComprar1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnComprar1.Location = New System.Drawing.Point(394, 31)
+        Me.BtnComprar1.Name = "BtnComprar1"
+        Me.BtnComprar1.Size = New System.Drawing.Size(75, 23)
+        Me.BtnComprar1.TabIndex = 3
+        Me.BtnComprar1.Text = "Comprar"
+        Me.BtnComprar1.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.ClientSize = New System.Drawing.Size(512, 568)
+        Me.Controls.Add(Me.BtnCerrar)
+        Me.Controls.Add(Me.Panel5)
+        Me.Controls.Add(Me.Panel4)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.lblAño)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.LblAlbum)
+        Me.Controls.Add(Me.LblNombreArtista)
+        Me.Controls.Add(Me.LblNombreCancion)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        Me.Panel4.ResumeLayout(False)
+        Me.Panel4.PerformLayout()
+        Me.Panel5.ResumeLayout(False)
+        Me.Panel5.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents LblNombreCancion As Label
+    Friend WithEvents LblNombreArtista As Label
+    Friend WithEvents LblAlbum As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents lblAño As Label
+    Friend WithEvents BtnVerInfo1 As Button
+    Friend WithEvents LblNombreArtistaSec1 As Label
+    Friend WithEvents LblNombreCancionSec1 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents LblNombreArtistaSec2 As Label
+    Friend WithEvents LblNombreCancionSec2 As Label
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents LblNombreArtistaSec3 As Label
+    Friend WithEvents LblNombreCancionSec3 As Label
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents LblNombreArtistaSec4 As Label
+    Friend WithEvents LblNombreCancionSec4 As Label
+    Friend WithEvents Panel5 As Panel
+    Friend WithEvents LblMostrarTotal As Label
+    Friend WithEvents LblTotal As Label
+    Friend WithEvents BtnCerrar As Button
+    Friend WithEvents BtnComprar1 As Button
+    Friend WithEvents BtnComprar2 As Button
+    Friend WithEvents BtnVerInfo2 As Button
+    Friend WithEvents BtnComprar3 As Button
+    Friend WithEvents BtnVerInfo3 As Button
+    Friend WithEvents BtnComprar4 As Button
+    Friend WithEvents BtnVerInfo4 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub BtnCerrar_Click(sender As Object, e As EventArgs) Handles BtnCerrar.Click
+
+    End Sub
+
+    Private Sub BtnVerInfo1_Click(sender As Object, e As EventArgs) Handles BtnVerInfo1.Click
+        LblNombreCancion.Text = LblNombreCancion.Text + " Far From th End of the World"
+    End Sub
+End Class
